64d2b9b9b8e3c58308306af34ceec3d30f1e3865
jha <j.haimez@vattenfall.fr>
2025-01-14 19:51:16 +0100

    update ajout acces au service depuis tout les services

--- src/ReaService.ts
+++ src/ReaService.ts
@@ -1,10 +1,13 @@
 import {RxService} from "./RxService";
 
 export abstract class ReaService<T, R extends string> {
+    protected rxService: RxService<R>;
+    
     protected constructor(key: R, rxService: RxService<R>) {
         if (rxService.hasService(key)) {
             console.warn(`Service with key "${key}" already exists. Overwriting.`);
         }
         rxService.addService<T>(key, this);
+        this.rxService = rxService;
     }
 }
