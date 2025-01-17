b9c29600801e95395de2330673d1609fa53d1d1c
jha <j.haimez@vattenfall.fr>
2025-01-17 06:10:29 +0100

    update : change export feature

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,11 @@
 export { ReaXor } from './ReaXor';
 export { ReaService } from "./ReaService";
 export { rea } from './Rea';
+export { getService } from './GetService';
 
 // Export React hooks
 export { useRea } from './ReaHook';
 export { useReaCompute } from './ReaHook';
-export { getService } from './ReaHook';
 export { useRxCompute } from './ReaHook';
 export { useRxFetch } from './ReaHook';
 export { useRxStore } from './ReaHook';
-
-// Export types
-export type { ReaXarType } from './type.d';
-export type { ReaXorType } from './type.d';
-export type { ReaType } from './type.d';
-export type { UseReaType } from './type.d';
-export type { UseReaComputeType } from './type.d';
-export type { RxStoreType } from './type.d';
-export type { RxServiceType } from './type.d';
